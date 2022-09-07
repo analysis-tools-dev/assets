c9ba8043851fca1aceb9a7383ef65cee112f7397
Matthias <matthias-endler@gmx.net>
2022-09-07 18:38:10 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -22,55 +22,99 @@ const throttledScreenshot = limiter.wrap(captureWebsite.file);
 const TOOLS_JSON_FILE =
   "https://raw.githubusercontent.com/analysis-tools-dev/static-analysis/master/data/api/tools.json";
 
-const getScreenshot = async (url: string, name: string) => {
-  const outDir = "screenshots";
-  const outPath = `${outDir}/${name}.jpg`;
-
-  if (fs.existsSync(outPath)) {
-    // Keep file if it is newer than `MAX_SCREENSHOT_AGE`
-    const fileAge = new Date().getTime() - fs.statSync(outPath).mtime.getTime();
-    if (fileAge < MAX_SCREENSHOT_AGE) {
-      return;
-    }
+const collectUrls = (tool: any) => {
+  const urls = [tool["homepage"]];
+  if (tool["source"] && tool["source"] !== tool["homepage"]) {
+    urls.push(tool["source"]);
+  }
+  for (const resource of tool["resources"] || []) {
+    urls.push(resource["url"]);
   }
 
+  return urls.filter((url) => url.includes("youtube") || url.includes("owasp"));
+  // return urls;
+};
+
+// Check if screenshot is newer than `MAX_SCREENSHOT_AGE`
+const isFreshScreenshot = (outPath: string) => {
+  const fileAge = new Date().getTime() - fs.statSync(outPath).mtime.getTime();
+  return fileAge < MAX_SCREENSHOT_AGE;
+};
+
+const fetchScreenshots = async (urls: string[], outDir: string) => {
   const screenshotOptions: FileOptions = {
     width: 1280,
     scaleFactor: 1.0,
     type: "jpeg",
     quality: 0.95,
-    timeout: 30,
-    delay: 2,
+    timeout: 60,
     overwrite: true,
     darkMode: true,
     waitForElement: undefined,
+    removeElements: [
+      "#onetrust-consent-sdk",
+      ".CookieConsent",
+      "#pz-gdpr",
+      ".cookie-disclaimer",
+      ".cookie-banner",
+      ".cookie-notice",
+      ".cookie-policy",
+      ".cookie-popup",
+      ".cookie-accept",
+      ".cookie-accepts",
+      ".cookie-acceptance",
+      ".cookie-acceptance-banner",
+      ".cookie-acceptance-container",
+      ".cookie-acceptance-overlay",
+      ".cookie-acceptance-wrapper",
+      ".cookie-accepter",
+      ".cookie-acceptor",
+      "#disclaimer",
+      "#disclaimer-container",
+      ".disclaimer",
+      ".disclaimer-container",
+      "tp-yt-iron-overlay-backdrop", // youtube
+      "#lightbox", // youtube
+      "[id*='sp_message_container']",
+    ],
   };
 
-  if (url.includes("github.com")) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    screenshotOptions.waitForElement = "#readme";
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    screenshotOptions.scrollToElement = "#readme";
-  }
+  // iterate over all urls with index
+  for (const [index, url] of urls.entries()) {
+    const outPath = `${outDir}${index}.jpg`;
 
-  console.log(`Fetching screenshot for ${url}`);
-  try {
-    fs.mkdirSync(outDir, { recursive: true });
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await throttledScreenshot(url, outPath, screenshotOptions);
-  } catch (err) {
-    console.log(`Error fetching screenshot for ${url}: ${err}`);
+    if (fs.existsSync(outPath) && isFreshScreenshot(outPath)) {
+      console.log(`Screenshot for ${url} is fresh. Skipping.`);
+      continue;
+    }
+
+    if (url.includes("github.com")) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      screenshotOptions.waitForElement = "#readme";
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      screenshotOptions.scrollToElement = "#readme";
+    }
+
+    console.log(`Fetching screenshot for ${url} to ${outPath}`);
+    try {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      await throttledScreenshot(url, outPath, screenshotOptions);
+    } catch (err) {
+      console.log(`Error fetching screenshot for ${url}: ${err}`);
+    }
   }
 };
 
 const response = await fetch(TOOLS_JSON_FILE);
-const body = await response.text();
-const json = JSON.parse(body);
+const json: any = await response.json();
+
 for (const tool in json) {
-  const homepage = json[tool]["homepage"];
-  console.log(`Fetching ${homepage}...`);
-  getScreenshot(homepage, tool);
+  const outDir = `screenshots/${tool}/`;
+  fs.mkdirSync(outDir, { recursive: true });
+
+  const urls = collectUrls(json[tool]);
+  fetchScreenshots(urls, outDir);
 }
